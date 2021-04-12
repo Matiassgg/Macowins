859a1f0424e34007743c92e08f8b0693f868ce05
Matiassgg <matugrillo@hotmail.com>
2021-04-11 21:19:30 -0300

    Agrego descripción de Clase Negocio

--- src/main/java/Negocio.java
+++ src/main/java/Negocio.java
@@ -2,10 +2,18 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
-
+/**
+ * El negocio se encarga de las ventas.
+ * Como se pedia ganancia para un dia particular,
+ * se decidió tomar esa fecha de la fecha de venta.
+ */
 public class Negocio {
   List<Venta> ventas = new ArrayList<>();
 
+  /**
+   * <b>Es la ganancia del negocio para una cierta fecha</b>
+   * @param fecha
+   */
   public double ganancias(Date fecha) {
     return ventas
         .stream()
